86f43fc6b9d5cb160f94427f0949437ee3a93148
Samira Fadi El Samargi <samiraelsamargi2018@gmail.com>
2020-11-14 09:54:05 +0000

    Automatically backed up by Learn

--- demo.js
+++ demo.js
@@ -11,8 +11,7 @@ let colorStates = {
 };
 
 // STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
-// Without JavaScript, clicking on these heart shapes does nothing. Uncomment
-// this code and refresh the demo page.
+ /Without JavaScript, clicking on these heart shapes does nothing. Uncomment this code and refresh the demo page./
 
 // let articleHearts = document.querySelectorAll(".like-glyph");
 
